5a17e16349e56d89e4d03e87389fdb96632fa8b5
virus <virusbb001a@gmail.com>
2013-06-04 08:18:45 +0900

    convert.plの文字コードとかの修正 引数を2つ与えるとstdoutに結果出力

--- tools/convert.pl
+++ tools/convert.pl
@@ -4,44 +4,80 @@ use strict;
 use warnings;
 use utf8;
 
-use JSON qw/encode_json/ ;
+use JSON qw/encode_json decode_json/ ;
+use Encode;
 
 #問題<>回答
 
-my ($tmp,$filename,$inputFileName);
+my ($tmp,$filename,$inputFileName,$metafile);
 my %data=();
 my @questionsList;
 my $jsonStr;
+my $flag=0;
 
-print("Input File Name> ");
-$inputFileName=<STDIN>;
+$flag=( defined($ARGV[0]) && defined($ARGV[1]) );
+
+if(!defined($ARGV[0])){
+ print("Input File Name> ");
+ $inputFileName=<STDIN>;
+ chomp($inputFileName);
+}else{
+ $inputFileName=$ARGV[0];
+}
 open(FH,"< $inputFileName") || die("File Open Error");
 
-print("Output File Name> ");
-$filename=<STDIN>; 
-open(OUTFILE,">$filename")||die ("Cannot open $filename");
+if(! $flag ){
+ print("Output File Name> ");
+ $filename=<STDIN>; 
+ chomp($filename);
+ open(OUTFILE,">$filename") || die ("Cannot open $filename");
+}
 
-print("YOUR NAME> ");
-$tmp=<STDIN>;
-chomp($tmp);
-$data{"Auther"}=$tmp;
-print("Question ID(string)> ");
-$tmp=<STDIN>;
-chomp($tmp);
-$data{"QuestionID"}=$tmp;
-print("Question Name> ");
-$tmp=<STDIN>;
-chomp($tmp);
-$data{"QuestionName"}=$tmp;
-print("This Question's version> ");
-$tmp=<STDIN>;
-chomp($tmp);
-$data{"version"}=$tmp;
+if(!defined($ARGV[1])){
+ print("Metadata File Name> ");
+ $metafile=<STDIN>;
+ if(!(open(META,"< $metafile"))){
+  print("METAFILE LOAD ERROR\n");
+  print("INPUT MANUALLY\n");
+  print("AUTHER NAME> ");
+  $tmp=<STDIN>;
+  chomp($tmp);
+  $data{"Auther"}=$tmp;
+  print("Question ID(string)> ");
+  $tmp=<STDIN>;
+  chomp($tmp);
+  $data{"QuestionID"}=$tmp;
+  print("Question Name> ");
+  $tmp=<STDIN>;
+  chomp($tmp);
+  $data{"QuestionName"}=$tmp;
+  print("This Question's version> ");
+  $tmp=<STDIN>;
+  chomp($tmp);
+  $data{"version"}=$tmp;
+  $data{"memo"}="備考";
+ }else{
+  my @text=<META>;
+  my $str=join('',@text);
+  $str=Encode::decode_utf8($str);
+  my $metaData=decode_json($str);
+  $data{"version"}=$metaData->{"version"};
+  $data{"Auther"}=$metaData->{"Auther"};
+  $data{"QuestionName"}=$metaData->{"QuestionName"};
+  $data{"QuestionIDName"}=$metaData->{"QuestionIDName"};
+  if(defined($metaData->{"memo"})){
+   $data{"memo"}=$metaData->{"memo"};
+  }else{
+   $data{"memo"}="備考";
+  }
+ }
+}
 
-$data{"memo"}="備考";
-
-print("FILE LOADING...\n");
+if(! $flag){
+ print("FILE LOADING...\n");
+}
 while(my $line = <FH>){
+ $line=Encode::decode_utf8($line);
  my ($question,$answer);
  my %questionData;
  my @tmp;
@@ -56,10 +92,12 @@ $data{"question"}=\@questionsList;
 close(FH);
 
 $jsonStr= encode_json(\%data);
-print($jsonStr);
 
+if(! $flag){
+ print OUTFILE $jsonStr;
+ close(OUTFILE);
+}else{
+ print $jsonStr;
+}
 print("\n");
-
-print OUTFILE $jsonStr;
-close(OUTFILE);
 
