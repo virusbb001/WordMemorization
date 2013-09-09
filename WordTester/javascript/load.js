17f720f48741a3a1218ae7110f8113f15482be51
virus <virusbb001a@gmail.com>
2013-09-09 13:20:21 +0900

    cssを先に読み込むようにした

--- WordTester/javascript/load.js
+++ WordTester/javascript/load.js
@@ -1,4 +1,8 @@
-var dataFileList=[
+loadExternalFile([
+ {
+  type:"css",
+  list:["//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css","/~virus/lib/bootstrap/css/bootstrap.min.css"]
+ },
  {
   type:"javascript",
   list:["//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js","/~virus/lib/jquery.js"]
@@ -8,13 +12,7 @@ var dataFileList=[
   list:["//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js","/~virus/lib/bootstrap/js/bootstrap.min.js"]
  },
  {
-  type:"css",
-  list:["//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css","/~virus/lib/bootstrap/css/bootstrap.min.css"]
- },
- {
   type:"javascript",
   list:["./javascript/main.js"]
  }
-];
-
-loadExternalFile(dataFileList);
+]);
